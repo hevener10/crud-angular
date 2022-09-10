d8da8019a227569807b3a2032eac96af0945308e
Hevener Ancelmo Pereira <hevener2@hotmail.com>
2022-09-10 12:08:33 -0300

    Criando Service

--- frontend/src/app/components/product/product-creat/product-creat.component.ts
+++ frontend/src/app/components/product/product-creat/product-creat.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-product-creat',
-  templateUrl: './product-creat.component.html',
-  styleUrls: ['./product-creat.component.css']
+  selector: "app-product-creat",
+  templateUrl: "./product-creat.component.html",
+  styleUrls: ["./product-creat.component.css"],
 })
 export class ProductCreatComponent implements OnInit {
+  constructor(private productService: ProductService,
+    private router:Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  createProduct(): void {
+    this.productService.showMessage("Produto Criado");
   }
-
+  cancelar(): void {
+    this.router.navigate(["/products"]);
+  }
 }
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
+  constructor(private snackBar: MatSnackBar) {}
 
-  constructor() { }
+  showMessage(msg: string): void {
+    console.log(msg);
+    this.snackBar.open(msg, "", {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    });
+  }
 }

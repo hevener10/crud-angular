cb75f6f43e38e42a6dab7b9fb7106628b4b60e82
Hevener Ancelmo Pereira <hevener2@hotmail.com>
2022-09-10 13:25:03 -0300

    Inicio Formulario cadastro Produtos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,6 +21,12 @@ import { ProductCreatComponent } from './components/product/product-creat/produc
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,7 +48,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/product/product-creat/product-creat.component.ts
+++ frontend/src/app/components/product/product-creat/product-creat.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from "@angular/core";
 import { ProductService } from "../product.service";
 import { Router } from '@angular/router';
+import { Product } from "../product.model";
+
 @Component({
   selector: "app-product-creat",
   templateUrl: "./product-creat.component.html",
   styleUrls: ["./product-creat.component.css"],
 })
 export class ProductCreatComponent implements OnInit {
+
+  product: Product = {
+    name: "",
+    price: 0
+
+  }
   constructor(private productService: ProductService,
     private router:Router) {}
 
   ngOnInit(): void {}
 
   createProduct(): void {
-    this.productService.showMessage("Produto Criado");
+    this.productService.create(this.product).subscribe(()=>{
+      this.productService.showMessage("Produto Criado");
+      this.router.navigate(["/products"]);
+    });
   }
   cancelar(): void {
     this.router.navigate(["/products"]);
